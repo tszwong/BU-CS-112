71cc1d353761632886d2fb7ca44038aec5446d11
Tsz Kit Wong <95050658+tszwong@users.noreply.github.com>
2022-05-21 16:32:04 -0400

    Add files via upload

---
+++ ps1/Ladder.java
@@ -0,0 +1,29 @@
+/*
+
+*/
+
+import java.util.*; 
+
+public class Ladder {
+    public static void main (String args[]) {
+       
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("height of ladder: ");
+        int height = scan.nextInt();
+        
+        System.out.print("angle in degrees of ladder: ");
+        int angle = scan.nextInt();
+        
+        double d_angle = angle;
+        double radians = (d_angle * Math.PI) / 180.00;
+        double length = height/(Math.sin(radians));
+        double yards = length / 3;
+        double remainder_feet = length % 3;
+
+        System.out.println("The required length is:\n" + length + " feet\n" + yards + " yards");
+        System.out.println((int)yards + " yards and " + remainder_feet + " feet");
+        
+        scan.close();
+    }
+}
